f287ef0de75f82f5a13512476cec4364af9b1a4f
mrstarkng <22127135@student.hcmus.edu.vn>
2024-12-01 23:17:36 +0700

    Save current changes before pull

--- front-end/src/pages/Contact.jsx
+++ front-end/src/pages/Contact.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Title from '../components/Title'
 import { assets } from '../assets/assets'
+import NewsletterBox from '../components/NewsletterBox'
 const contact = () => {
   return (
     <div>
@@ -11,8 +12,19 @@ const contact = () => {
 
       <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28'>
         <img className='w-full md:max-w-[450px]' src={assets.contact_img} alt="contact" />
+        <div className='flex flex-col justify-center items-start gap-6'>
+          <p className='font-semibold text-xl text-gray-800'>Our Store</p>
+          <p className='text-gray-500'>Nguyen Van Cu, Ward 4<br />District 4, Ho Chi Minh City</p>
+          <p className='text-gray-500'>Tel: (+84)906123456 <br />Email: fashionfit@student.hcmus.edu.vn</p>
+          <p className='font-semibold text-xl text-gray-800'>Careers at FashionFit</p>
+          <p className='text-gray-500'>Learn more about our open positions and career opportunities</p>
+          <button className='bg-gray-800 text-white px-4 py-2 rounded-md hover:bg-gray-900'>Join Our Team</button>
+
+        </div>
 
       </div>
+
+      <NewsletterBox />
       
 
     </div>
