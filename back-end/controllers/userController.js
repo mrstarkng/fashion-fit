4219764a36987b34e60042043852f1686c5c27cd
mrstarkng <22127135@student.hcmus.edu.vn>
2024-12-09 23:46:38 +0700

    Save changes before end

--- back-end/controllers/userController.js
+++ back-end/controllers/userController.js
@@ -1,4 +1,5 @@
-
+import validator from "validator";
+import userModel from "../models/userModel.js"
 //route for login user
 
 const loginUser = async (req, res) => {
@@ -9,7 +10,16 @@ const loginUser = async (req, res) => {
 //route for register user
 const registerUser = async (req, res) => {
    try {
-    const { name, email, password } = req.body
+       const { name, email, password } = req.body;
+       //checking
+       const exists = await userModel.findOne({ email });
+       if (exists) {
+           return res.json({success:false, message:"User already exists"})
+       }
+
+       if (condition) {
+           
+       }
    } catch (error) {
     res.status(500).json({message: error.message})
    }
